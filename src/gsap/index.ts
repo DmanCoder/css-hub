d3b048bdc6fd02c89de8f7201f434ed34e53451e
DmanCoder <ktdenis.1@live.com>
2022-08-16 20:28:10 +1000

    removed

--- src/gsap/index.ts
+++ src/gsap/index.ts
@@ -2,9 +2,8 @@ import { gsap } from 'gsap';
 import { Draggable } from 'gsap/Draggable';
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { InertiaPlugin } from 'gsap/InertiaPlugin';
 
-gsap.registerPlugin(InertiaPlugin, Draggable, ScrollToPlugin, ScrollTrigger);
+gsap.registerPlugin(Draggable, ScrollToPlugin, ScrollTrigger);
 
 gsap.config({
   nullTargetWarn: false,
