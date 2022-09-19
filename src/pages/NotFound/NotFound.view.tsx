23cab6e013d6e2defd33dfff5e250a7352b657f9
denis.otim <denis.otim@betmakers.com>
2022-09-20 08:48:42 +1000

    Push back to home page if no route is found

--- src/pages/NotFound/NotFound.view.tsx
+++ src/pages/NotFound/NotFound.view.tsx
@@ -1,39 +1,10 @@
 import React from 'react';
 
-import Hero from '../../components/Hero';
-import Gallery from '../../components/Gallery';
-
 import { INotFoundProps } from './NotFound.types';
-import * as S from './NotFound.styled';
-import utils from '../../utils';
-import useHome from './useNotFound';
 
 const NotFound: React.FC<INotFoundProps> = (): JSX.Element => {
-  const { tabs, myList } = useHome();
-
-  return (
-    <S.Wrapper>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-      <h3 style={{ color: 'wite' }}>PAGE NOT FOUND</h3>
-    </S.Wrapper>
-  );
+  location.href = '/'; // Push back to home page
+  return <React.Fragment />;
 };
 
 export default NotFound;
