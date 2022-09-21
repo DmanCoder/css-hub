54e8dd5ee116a641fc7d6e73b1a9ebbbd5f5b0e0
DmanCoder <ktdenis.1@live.com>
2022-09-21 20:21:07 +1000

    Added new network id

--- src/typescriptGlobals/constants.ts
+++ src/typescriptGlobals/constants.ts
@@ -10,6 +10,8 @@ export const APPLE_TV_NETWORK_ID = 2552 as const;
 
 export const DISNEY_NETWORK_ID = 2739 as const;
 
+export const CRUNCHYROLL_NETWORK_ID = 1112 as const;
+
 export const SHUFFLE_NETWORK_ID = -1 as const;
 
 export const COUNTRY_ALPHA_CODES_2 = {
