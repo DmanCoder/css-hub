8f261a1c7d0fe4d7aa8c8261b4f837e5eda9a13b
denis.otim <denis.otim@betmakers.com>
2022-09-28 08:40:47 +1000

    Added hero element

--- src/pages/MediaDetails/MediaDetails.view.tsx
+++ src/pages/MediaDetails/MediaDetails.view.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
+import Hero from '../../components/Hero';
 
 import { IMediaDetailsProps } from './MediaDetails.types';
 
 const MediaDetails: React.FC<IMediaDetailsProps> = (): JSX.Element => {
   return (
     <div>
-      <h3>DETAILS</h3>
-      <h3>DETAILS</h3>
-      <h3>DETAILS</h3>
-      <h3>DETAILS</h3>
-      <h3>DETAILS</h3>
-      <h3>DETAILS</h3>
-      <h3>DETAILS</h3>
-      <h3>DETAILS</h3>
-      <h3>DETAILS</h3>
+      <Hero />
     </div>
   );
 };
