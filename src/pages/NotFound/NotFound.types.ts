456c31b1f279c34c5f3178ce2138d694e8984eb2
denis.otim <denis.otim@betmakers.com>
2022-10-18 08:34:04 +1000

    removed

--- src/pages/NotFound/NotFound.types.ts
+++ src/pages/NotFound/NotFound.types.ts
@@ -1,8 +1,1 @@
-import { GalleryTypes, MediaTypes } from '../../typescriptGlobals/types';
-
 export interface INotFoundProps {}
-
-export type UseNotFoundReturnTypes = {
-  myList: MediaTypes[];
-  tabs: GalleryTypes[];
-};
