0e9e16f8966f28f27996eff3e4342f62a523c716
DmanCoder <ktdenis.1@live.com>
2023-02-15 16:28:17 +1000

    Added todo

--- src/pages/Home/Home.view.tsx
+++ src/pages/Home/Home.view.tsx
@@ -12,6 +12,8 @@ import Hero from '../../component/Hero';
 // TODO: https://www.npmjs.com/package/smooth-scrollbar
 // TODO: https://www.youtube.com/watch?v=mxVFEnbRsBw&ab_channel=MrCoder
 // TODO: https://www.youtube.com/watch?v=OvNG7bJjjiw&ab_channel=ALL.DESIGN (MAIN)
+// TODO: https://greensock.com/forums/topic/32612-scroll-trigger-momentum-scrolling-not-anchor-link/ (MAIN)
+// TODO: https://greensock.com/docs/v3/Plugins/ScrollSmoother (AYYY YES)
 const Home: React.FC<IHomeProps> = (): JSX.Element => {
   const { test } = useHome();
   console.log(test, 'testtest');
